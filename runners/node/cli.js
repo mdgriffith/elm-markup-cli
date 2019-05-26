57fda6928af3c72488a1a0fe31af90a0d0828365
Matthew Griffith <mdg.griffith@gmail.com>
2019-05-26 17:51:40 -0400

    show help text if args aren't understood.

--- runners/node/cli.js
+++ runners/node/cli.js
@@ -3,18 +3,46 @@
 "use strict";
 
 var path = require('path')
-
+const process = require('process');
+const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
+const Mark = require("./lib.js");
 const optionDefinitions = [
   { name: 'report', type: String, defaultValue: 'console' },
   { name: 'version', type: Boolean }
 ]
 
+function log_help() {
+  const usage = commandLineUsage([
+    {
+      header: 'Elm Markup',
+      content: 'Find and parse *.emu files using any exposed `Mark.Document` you have in your Elm project.'
+    },
+    {
+      header: 'Options',
+      optionList: optionDefinitions
+    },
+    {
+      content: 'Read more: {underline https://github.com/mdgriffith/elm-markup-cli}'
+    }
+  ])
+  console.log(usage)
+}
 
-const Mark = require("./lib.js");
-const commandLineArgs = require('command-line-args');
-const options = commandLineArgs(optionDefinitions);
 
-if (options.version) {
+var options = null
+try {
+  options = commandLineArgs(optionDefinitions);
+}
+catch (err) {
+  log_help()
+  process.exit()
+}
+
+if (options.help) {
+  log_help()
+}
+else if (options.version) {
   console.log(require(path.join(__dirname, '..', '..', 'package.json')).version);
 } else {
   switch (options.report) {
@@ -30,3 +58,4 @@ if (options.version) {
       console.log('--report needs to be either `json` or not be present.')
   }
 }
+
